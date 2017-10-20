505ebe20995cbdd9c6042fbcdd8642ec29ce24d9
GilsondaGama <32863365+GilsondaGama@users.noreply.github.com>
2017-10-20 07:58:25 -0200

    Add files via upload

---
+++ modeloBeans/BeansFilhos.java
@@ -0,0 +1,117 @@
+
+package modeloBeans;
+
+public class BeansFilhos {
+
+
+    private int id;
+    private String nome;
+    private int idMae;
+    private String responsavel;
+    private String endereco;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String nascimento;
+    private String sexo;
+    private String contato1;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdMae() {
+        return idMae;
+    }
+
+    public void setIdMae(int idMae) {
+        this.idMae = idMae;
+    }
+
+    public String getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(String responsavel) {
+        this.responsavel = responsavel;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getContato1() {
+        return contato1;
+    }
+
+    public void setContato1(String contato1) {
+        this.contato1 = contato1;
+    } 
+    
+ 
+}
---
+++ modeloBeans/BeansMaes.java
@@ -0,0 +1,125 @@
+
+package modeloBeans;
+
+public class BeansMaes {
+
+
+    private int id;
+    private String nome;
+    private String endereco;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String nascimento;
+    private String cpf;
+    private String rg;
+    private String contato1;
+    private String contato2;
+    private int filhos;      
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getContato1() {
+        return contato1;
+    }
+
+    public void setContato1(String contato1) {
+        this.contato1 = contato1;
+    }
+
+    public String getContato2() {
+        return contato2;
+    }
+
+    public void setContato2(String contato2) {
+        this.contato2 = contato2;
+    }
+
+    public int getFilhos() {
+        return filhos;
+    }
+
+    public void setFilhos(int filhos) {
+        this.filhos = filhos;
+    }
+ 
+}
