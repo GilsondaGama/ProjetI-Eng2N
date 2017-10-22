2d1539c460de946d490365e8266debf6f2f41205
GilsondaGama <32863365+GilsondaGama@users.noreply.github.com>
2017-10-22 10:56:12 -0200

    Add files via upload

---
+++ BD Estrutura e Dados/pastoralinfo_filho.sql
@@ -0,0 +1,65 @@
+CREATE DATABASE  IF NOT EXISTS `pastoralinfo` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `pastoralinfo`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: pastoralinfo
+-- ------------------------------------------------------
+-- Server version	5.7.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `filho`
+--
+
+DROP TABLE IF EXISTS `filho`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `filho` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `idMae` int(11) NOT NULL,
+  `responsavel` varchar(100) NOT NULL,
+  `endereco` varchar(60) DEFAULT NULL,
+  `complemento` varchar(45) DEFAULT NULL,
+  `bairro` varchar(35) DEFAULT NULL,
+  `cidade` varchar(30) DEFAULT NULL,
+  `estado` varchar(2) DEFAULT NULL,
+  `nascimento` date DEFAULT NULL,
+  `sexo` varchar(9) DEFAULT NULL,
+  `contato1` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idMae_idx` (`idMae`),
+  CONSTRAINT `idMae` FOREIGN KEY (`idMae`) REFERENCES `mae` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `filho`
+--
+
+LOCK TABLES `filho` WRITE;
+/*!40000 ALTER TABLE `filho` DISABLE KEYS */;
+INSERT INTO `filho` VALUES (1,'Antonio',3,'Raimuda Soares','Rua Laranja','Apt 1','Centro','Macapá','AP',NULL,'','9698565-9865'),(3,'DSCFSDFCDSFDSF',27,'','DSFSDF','','','','  ',NULL,NULL,'(  )     -    '),(4,'Ricardo',12,'Carlinha','Ru Alvor','Alvorada','centro','macapá','AP',NULL,NULL,'(98) 6546-4946'),(5,'Maria',11,'Meire','ende','comp','bair','cid','MA',NULL,'Feminino','(88) 8888-8888'),(6,'sadasfsadfsdafafd',26,'','','','','','  ',NULL,'Feminino','(  )     -    '),(7,'ddsvdvsdvsv',26,'','','','','','  ','2017-10-05','Masculino','(  )     -    ');
+/*!40000 ALTER TABLE `filho` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-10-22  9:54:21
